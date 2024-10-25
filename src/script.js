1517fbdf07011c3702f8dcc48eb8b5696964b053
Shiv <shivbhagat@icloud.com>
2024-10-25 15:22:32 -0500

    started from excel to statement

--- src/script.js
+++ src/script.js
@@ -3,23 +3,14 @@
 ("use strict");
 
 /*
-[0] : faculty code
-[1] : student number
-[2] : student last name
-[3] : student first name
-[9] : external school code
-[10] : external school name
-[11] : effective term for that course
-[12] : external course name
-[13] : external course number
-[14] : external course title
-[15] : external credit hours
-[16] : external grade
-[17] : UM course code
-[18] : UM course number
-[19] : UM course title
-[20] : UM credit hours
-[21] : UM grade
+for each line:
+[0] : external course code
+[1] : external course title
+[2] : external grade
+[3] : UM course code
+[4] : UM course title
+[5] : UM credit hours
+[8] : UM grade
 
 */
 
@@ -28,12 +19,12 @@ class CourseEquivalency {
 		this.schoolCode = "";
 		this.schoolName = "";
 		this.term = "";
-		this.exCourseName = "";
-		this.exCourseNum = "";
+		this.exCourseCode = "";
+		// this.exCourseNum = "";
 		this.exCourseTitle = "";
 		this.exGrade = "";
-		this.umCourseName = "";
-		this.umCourseNum = "";
+		this.umCourseCode = "";
+		// this.umCourseNum = "";
 		this.umCourseTitle = "";
 		this.umCredits = "";
 		this.umGrade = "";
@@ -80,7 +71,7 @@ class Student {
 
 document
 	.getElementById("generate-button")
-	.addEventListener("click", generateEmail);
+	.addEventListener("click", generateData);
 document.getElementById("clear-button").addEventListener("click", clearData);
 
 //hide back to top when its in the #input-section
@@ -103,13 +94,16 @@ function clearData() {
 	resetVariables();
 }
 
-let student = new Student();
+let student = new Student(),
+	hasStudentNumber = false,
+	hasStudentName = false,
+	hasProgram = false;
 
 function resetVariables() {
 	student = new Student();
 }
 
-function generateEmail() {
+function generateData() {
 	resetVariables();
 	inputText = document.getElementById("input-textarea").value;
 
@@ -120,64 +114,135 @@ function generateEmail() {
 	}
 
 	// Split the input text into lines
-	let lines = inputText.split("\n");
-	console.log(lines[1]);
+	let splitText = inputText.split("\n");
 
-	// Extract student number
-	primaryData = lines[1].split("\t");
-	student.studentNumber = "00" + primaryData[1];
-
-	// Extract student name
-	if (primaryData[2] != "") {
-		student.name = primaryData[2] + " ";
-	}
-	if (primaryData[3] != "") {
-		student.name += primaryData[3];
-	}
-
-	//run through each lines
-	for (let i = 1; i < lines.length; i++) {
-		let data = lines[i].split("\t");
-		let course = new CourseEquivalency();
-
-		// Extract school code
-		course.schoolCode = data[9];
-
-		// Extract school name
-		course.schoolName = data[10];
-
-		// Extract course details
-		course.term = data[11];
-		course.exCourseName = data[12];
-		course.exCourseNum = data[13];
-		course.exCourseTitle = data[14];
-		course.exGrade = data[16];
-		course.umCourseName = data[17];
-		course.umCourseNum = data[18];
-		course.umCourseTitle = data[19];
-		course.umCredits = data[20];
-		course.umGrade = data[21];
-
-		console.log(course.umCourseNum);
-
-		// Add course to student
-		if (course.exGrade === "LP") {
-			if (
-				course.umCourseNum == "9996" ||
-				course.umCourseNum == "9997" ||
-				course.umCourseNum == "9993"
-			) {
-				student.pushPendingDecision(course);
-			} else {
-				student.pushApproved(course);
-			}
-		} else if (course.exGrade == "LP PN") {
-			student.pushPendingApprv(course);
-		} else if (course.exGrade == "LP DN") {
-			student.pushDenied(course);
-		} else if (course.exGrade == "LP SS") {
-			student.pushSS(course);
+	// Extract student details
+	for (let i = 0; i < splitText.length; i++) {
+		//get student #
+		if (splitText[i].includes("Student #:") && hasStudentNumber == false) {
+			student.studentNumber = splitText[i].split(":")[1].trim();
+			hasStudentNumber = true;
 		}
+		//get student name
+		if (splitText[i].includes("Student Name:") && hasStudentName == false) {
+			student.name = splitText[i].split(":")[1].trim();
+			hasStudentName = true;
+		}
+		//get program
+		if (splitText[i].includes("Program:") && hasProgram == false) {
+			student.program = splitText[i].split(":")[1].trim();
+			hasProgram = true;
+		}
+
+		//get course data
+		if (splitText[i].includes("Course No.")) {
+			coursesData = splitText.slice(i + 1);
+			break;
+		}
+	}
+
+	let extSchoolName = "",
+		extSchoolCode = "",
+		line = [];
+
+	for (let i = 0; i < coursesData.length; i++) {
+		if (
+			coursesData[i] != undefined &&
+			!coursesData[i].includes("Attributes:") &&
+			!coursesData[i].includes("Comments:") &&
+			coursesData[i] != ""
+		) {
+			if (
+				coursesData[i].includes(" - ") &&
+				!coursesData[i].includes("\t")
+			) {
+				extSchoolName = coursesData[i].split(" - ")[0];
+				extSchoolCode = coursesData[i].split(" - ")[1];
+			} else {
+				line = coursesData[i].split("\t");
+				let course = new CourseEquivalency();
+				course.schoolCode = extSchoolCode;
+				course.schoolName = extSchoolName;
+				course.exCourseCode = line[0];
+				course.exCourseTitle = line[1];
+				course.exGrade = line[2];
+				course.umCourseCode = line[3];
+				course.umCourseTitle = line[4];
+				course.umCredits = line[5];
+				course.umGrade = line[8];
+
+				// Add course to student
+				if (course.exGrade == "LP") {
+					if (
+						course.umCourseCode.includes("9996") ||
+						course.umCourseCode.includes("9997") ||
+						course.umCourseCode.includes("9993")
+					) {
+						student.pushPendingDecision(course);
+					} else {
+						student.pushApproved(course);
+					}
+				} else if (course.exGrade == "LP PN") {
+					student.pushPendingApprv(course);
+				} else if (course.exGrade == "LP DN") {
+					student.pushDenied(course);
+				} else if (course.exGrade == "LP SS") {
+					student.pushSS(course);
+				}
+			}
+		} else if (
+			coursesData[i].includes("Where sufficient") ||
+			coursesData[i].includes("Courses transformed")
+		) {
+			break;
+		}
+
+		/*
+		//run through each lines
+		for (let i = 1; i < lines.length; i++) {
+			let data = lines[i].split("\t");
+			let course = new CourseEquivalency();
+
+			// Extract school code
+			course.schoolCode = data[9];
+
+			// Extract school name
+			course.schoolName = data[10];
+
+			// Extract course details
+			course.term = data[11];
+			course.exCourseName = data[12];
+			course.exCourseNum = data[13];
+			course.exCourseTitle = data[14];
+			course.exGrade = data[16];
+			course.umCourseName = data[17];
+			course.umCourseNum = data[18];
+			course.umCourseTitle = data[19];
+			course.umCredits = data[20];
+			course.umGrade = data[21];
+
+			console.log(course.umCourseNum);
+
+			// Add course to student
+			if (course.exGrade === "LP") {
+				if (
+					course.umCourseNum == "9996" ||
+					course.umCourseNum == "9997" ||
+					course.umCourseNum == "9993"
+				) {
+					student.pushPendingDecision(course);
+				} else {
+					student.pushApproved(course);
+				}
+			} else if (course.exGrade == "LP PN") {
+				student.pushPendingApprv(course);
+			} else if (course.exGrade == "LP DN") {
+				student.pushDenied(course);
+			} else if (course.exGrade == "LP SS") {
+				student.pushSS(course);
+			}
+		}
+			*/
 	}
 
 	console.log(student);
